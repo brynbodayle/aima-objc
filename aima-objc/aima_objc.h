9543d0af2ac1183d12e5549b6c4c2eaad22f0dd8
Bryn Bodayle <bryn.bodayle@gmail.com>
2012-10-30 09:51:02 -0500

    initial commit

---
+++ aima-objc/aima_objc.h
@@ -0,0 +1,13 @@
+//
+//  aima_objc.h
+//  aima-objc
+//
+//  Created by Bryn Bodayle on 10/30/12.
+//  Copyright (c) 2012 Bryn Bodayle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface aima_objc : NSObject
+
+@end
---
+++ aima-objc/aima_objc.m
@@ -0,0 +1,13 @@
+//
+//  aima_objc.m
+//  aima-objc
+//
+//  Created by Bryn Bodayle on 10/30/12.
+//  Copyright (c) 2012 Bryn Bodayle. All rights reserved.
+//
+
+#import "aima_objc.h"
+
+@implementation aima_objc
+
+@end
