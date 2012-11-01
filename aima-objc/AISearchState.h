3c53ba454e4b5e85c3edffc18ecc05a9f2d78aad
Bryn Bodayle <bryn.bodayle@gmail.com>
2012-11-01 13:05:13 -0500

    added initial search state class

---
+++ aima-objc/AISearchState.h
@@ -0,0 +1,27 @@
+//
+//  AISearchState.h
+//  aima-objc
+//
+//  Created by Bryn Bodayle on 11/1/12.
+//  Copyright (c) 2012 Bryn Bodayle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AISearchState : NSObject
+
+//initializer, this is used like a constructor
+-(id)initWithString:(NSString *)stateString;
+
+
+//replace toString and prettyPrint
+-(NSString *)description;
+-(NSString *)formattedDescription;
+
+-(id)copy;
+
+-(BOOL)isEqual:(id)object;
+
+-(NSComparisonResult)compareTo:(AISearchState *)object;
+
+@end
---
+++ aima-objc/AISearchState.m
@@ -0,0 +1,61 @@
+//
+//  AISearchState.m
+//  aima-objc
+//
+//  Created by Bryn Bodayle on 11/1/12.
+//  Copyright (c) 2012 Bryn Bodayle. All rights reserved.
+//
+
+#import "AISearchState.h"
+
+@implementation AISearchState
+
+
+-(id)initWithString:(NSString *)stateString
+{
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:[NSString stringWithFormat:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)]
+                                 userInfo:nil];
+
+}
+-(NSString *)description {
+    
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:[NSString stringWithFormat:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)]
+                                 userInfo:nil];
+}
+-(NSString *)formattedDescription {
+    
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:[NSString stringWithFormat:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)]
+                                 userInfo:nil];
+    
+}
+
+-(id)copy {
+    
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:[NSString stringWithFormat:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)]
+                                 userInfo:nil];
+
+    
+}
+
+-(BOOL)isEqual:(id)object {
+    
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:[NSString stringWithFormat:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)]
+                                 userInfo:nil];
+
+}
+
+-(NSComparisonResult)compareTo:(AISearchState *)object {
+    
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:[NSString stringWithFormat:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)]
+                                 userInfo:nil];
+
+    
+}
+
+@end
